7e6d31a6aff7fddaecfe3a173e669ca736cb969f
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-02-24 11:30:45 -0500

    Fix/constrains already exists

--- src/server.js
+++ src/server.js
@@ -2,10 +2,20 @@ import { createTables } from './config/postgres.js'
 import app from './app.js'
 import { env } from './config/env.js'
 
+
 try {
+    app.get('/init-db', async (req, res)=>{
+        try {
+            await createTables();
+            res.status(200).json({response : true , message : "Tables created successfylly"})
+        } catch (error) {
+            console.log(error);
+            res.status(500)
+        }
+    })
     console.log('Conecting to PostgresSQL...');
-    await createTables();
-    console.log('Successfully');
+    /*await createTables();
+    console.log('Successfully'); */
     app.listen(env.port, () => {
         console.log(`server running on port ${env.port}`);
     })
