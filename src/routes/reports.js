7adce0891a9c675774349c28ed93aa10c08b5b81
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-02-27 00:51:31 -0500

    Funcion y endpoint de reportes

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -4,7 +4,30 @@ import { reports } from "../services/reportsService.js";
 const router = Router()
 
 router.get('/revenue', async (req, res) => {
-    res.status(200).json(reports())
+    const r = await reports()
+    let totalRevenue = 0
+    r.forEach(i => totalRevenue += Number(i.total_revenue))
+    let respo = []
+    r.forEach(i => {
+        respo.push(
+            {
+                "insuranceName" : i.name,
+                "totalAmount": i.total_amount,
+                "appointmentCount" : i.appointment_count
+            }
+        )
+    })
+    res.status(200).json({
+        "ok": true,
+        "report": {
+            "totalRevenue": totalRevenue,
+            "byInsurance": respo,
+            "period": {
+                "startDate": "2024-01-01",
+                "endDate": "2024-12-31"
+            }
+        }
+    })
 })
 
 export default router;
--- src/services/reportsService.js
+++ src/services/reportsService.js
@@ -4,8 +4,8 @@ import { pool } from "../config/postgres.js";
 async function reports() {
     const client = await pool.connect()
     try {
-        await client.query('')
-        return console.log("reports");
+        const res = await client.query('select i.name, SUM(amount_paid) as total_revenue, count(a.id) as appointment_count,i.coverage_percentage ,ROUND(SUM(a.amount_paid) * ((100 - i.coverage_percentage)/100),2)as total_amount from "Appointment" a join "Insurance" i on a.insurance_id  = i.id group by i.id, i."name" ;')
+        return res.rows
     } catch (error) {
         console.error(er);
     }finally{
