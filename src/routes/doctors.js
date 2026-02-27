f530fb00eba8acd0a75be9c3d1aef7ebff5712ec
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-02-26 22:16:56 -0500

    Separacion de archivos de rutas

---
+++ src/routes/doctors.js
@@ -0,0 +1,66 @@
+import { Router } from 'express'
+import { getDoctorById, getDoctors, updateDoctor } from '../services/doctorsServices.js';
+
+const router = Router();
+
+//Doctors endpoint
+router.get('/', async (req, res) => {
+    try {
+        const doctors = await getDoctors();
+        res.status(200).json({
+            "ok": true,
+            "doctors": doctors
+        });
+    } catch (error) {
+        console.error("Doctors error:", error);
+        res.status(500).json({
+            success: false,
+            message: "Doctors failed"
+        });
+    }
+})
+
+//Doctor endpoint by id
+router.get('/:id', async (req, res) => {
+    try {
+        const doctor = await getDoctorById(req.params.id);
+        res.status(200).json({
+            "ok": true,
+            "doctor": {
+                "id": doctor.id,
+                "name": doctor.name,
+                "email": doctor.email,
+                "specialty": doctor.specialty,
+                "createdAt": doctor.createdAt
+            }
+        });
+    } catch (error) {
+        console.error("Doctor error:", error);
+        res.status(500).json({
+            success: false,
+            message: "Doctor failed"
+        });
+    }
+})
+
+//Update doctor endpoint
+
+//Falta la propagación de los cambios
+router.put('/:id', async (req, res) => {
+    try {
+        const doctor = await updateDoctor(req.params.id, req.body);
+        res.status(200).json({
+            "ok": true,
+            "message": "Doctor updated successfully",
+            "doctor": doctor
+            });
+    } catch (error) {
+        console.error("Doctor error:", error);
+        res.status(500).json({
+            success: false,
+            message: "Doctor failed"
+        });
+    }
+})
+
+export default router;
